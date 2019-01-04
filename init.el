9005b64c3318f7a55e76cc816755052673eab1df
Juan Pablo García Garland <jpgarcia@fing.edu.uy>
2019-01-04 18:10:43 -0300

    last version of init

--- init.el
+++ init.el
@@ -1,82 +1,80 @@
-(load "~/.emacs.d/lisp/PG/generic/proof-site")
-(load "~/.emacs.d/lisp/superEval/supereval")
-
-(load "~/.emacs.d/lisp/pretty/pretty-mode")
-(load "~/.emacs.d/lisp/patoline/patoline-mode")
-
 (load "~/.emacs.d/lisp/column-marker/column-marker")
+(load "~/.emacs.d/lisp/pml/pml2-mode")
 
-;;agda input
-(load "/usr/share/emacs/site-lisp/elpa-src/agda2-mode-2.5.1/agda2-mode")
+(add-to-list 'load-path "~/.emacs.d/lisp/hs-unicode")
+(require 'haskell-unicode-input-method)
 
 ;;; col marker
 (add-hook 'prog-mode-hook (lambda () (interactive) (column-marker-2 76)))
 (add-hook 'prog-mode-hook (lambda () (interactive) (column-marker-3 79)))
 
-;(add-to-list 'load-path "~/.emacs.d/lisp/")
-;(load "patoline-mode")
+(add-hook 'tex-mode-hook (lambda () (interactive) (column-marker-2 76)))
+(add-hook 'tex-mode-hook (lambda () (interactive) (column-marker-3 79)))
 
-(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-(add-hook 'haskell-mode-hook 'turn-on-pretty-mode)
-(add-hook 'literate-haskell-mode-hook 'interactive-haskell-mode)
-(add-hook 'literate-haskell-mode-hook 'turn-on-pretty-mode)
 
-;(add-hook 'proof-mode-hook (lambda () (set-input-method "TeX") ))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (tsdh-dark))) 
  '(column-number-mode t)
+ '(custom-safe-themes
+   (quote
+    ("7e3890e2f54212c6929eb6bab6024a9afd6414324e45ad48b6a6ec9fb2029306" default)))
+ '(fci-rule-color "#383838")
  '(indent-tabs-mode nil)
- '(line-number-mode t)
- '(tool-bar-mode nil))
+ '(linum-mode t t)
+ '(tool-bar-mode nil)
+ '(xterm-mouse-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
-(set-variable 'phox-prog-name
-              "usr/local/bin/phox -pg")
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight normal :height 98 :width normal)))))
+;(set-variable 'phox-prog-name
+;              "usr/local/bin/phox -pg")
 
 
-(require 'package) ;; You might already have this line
+
+(require 'package)
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
                     (not (gnutls-available-p))))
-       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
-  (add-to-list 'package-archives (cons "melpa" url) t))
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-(package-initialize) ;; You might already have this line
+       (proto (if no-ssl "http" "https")))
+  (add-to-list 'package-archives
+               (cons "melpa" (concat proto "://melpa.org/packages/")) t))
+(package-initialize)
+;; (package-refresh-contents) ;;;;;;
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/lisp/cyberpunk-theme.el/")
+(load-theme 'cyberpunk t)
+(add-to-list 'load-path "~/.emacs.d/elpa/mmm-mode-0.5.6/")
+
+;(require 'mmm-mode)
+(add-hook 'haskell-mode-hook 'my-mmm-mode)
+
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+;(add-hook 'idris-mode-hook 'my-mmm-mode)
+
+;interactive haskell mode
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
 
 
-(defun haskell-unicode ()
-  (interactive)
-  (substitute-patterns-with-unicode
-   (list (cons "\\(<-\\)" 'left-arrow)
-         (cons "\\(->\\)" 'right-arrow)
-         (cons "\\(==\\)" 'identical)
-         (cons "\\(/=\\)" 'not-identical)
-         (cons "\\(()\\)" 'nil)
-         (cons "\\<\\(sqrt\\)\\>" 'square-root)
-         (cons "\\(&&\\)" 'logical-and)
-         (cons "\\(||\\)" 'logical-or)
-         (cons "\\<\\(not\\)\\>" 'logical-neg)
-         (cons "\\(>\\)\\[^=\\]" 'greater-than)
-         (cons "\\(<\\)\\[^=\\]" 'less-than)
-         (cons "\\(>=\\)" 'greater-than-or-equal-to)
-         (cons "\\(<=\\)" 'less-than-or-equal-to)
-         (cons "\\<\\(alpha\\)\\>" 'alpha)
-         (cons "\\<\\(beta\\)\\>" 'beta)
-         (cons "\\<\\(gamma\\)\\>" 'gamma)
-         (cons "\\<\\(delta\\)\\>" 'delta)
-         (cons "\\(''\\)" 'double-prime)
-         (cons "\\('\\)" 'prime)
-         (cons "\\(!!\\)" 'double-exclamation)
-         (cons "\\(\\.\\.\\)" 'horizontal-ellipsis))))
+(defun my-mmm-mode ()
+  ;; go into mmm minor mode when class is given
+  (make-local-variable 'mmm-global-mode)
+  (setq mmm-global-mode 'true))
 
-(add-hook 'haskell-mode 'haskell-unicode)
+(setq mmm-submode-decoration-level 0)
+
+
+
+(setenv "PATH" (concat (getenv "PATH") ":~/.cabal/bin"))
+    (setq exec-path (append exec-path '("~/.cabal/bin")))
+
+(global-linum-mode 1)
+(global-auto-complete-mode 1)
+(global-column-enforce-mode 1)
+(global-highlight-parentheses-mode 1)
+(put 'downcase-region 'disabled nil)
